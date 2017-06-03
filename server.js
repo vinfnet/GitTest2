27ff1013ddf7773c28fdaabafb9adf9a704eb213
Simon <simon@FlackPackMac.local>
2017-06-03 20:36:52 +0100

    test2

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log('Got request for ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
+}).listen(process.env.PORT);
+
+// this is a comment
+//and have another one
